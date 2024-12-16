fb2d6acc2d1c21607f792d924f35588a7d5a0791
LuisRamajoCeu <lramajo2587@ceuandalucia.es>
2024-12-16 16:48:14 +0100

    Cabeceras

---
+++ Tema4/src/cabeceras/Cabeceras.java
@@ -0,0 +1,68 @@
+package cabeceras;
+
+public class Cabeceras {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static Integer suma(Integer numero1, Integer numero2) {
+		Integer suma = numero1 + numero2;
+		return suma;
+	}
+
+	public static Float area(Float base, Float altura) {
+		Float area = base * altura;
+		return area;
+	}
+
+	public static Boolean par(Integer n) {
+		Boolean par = n % 2 == 0;
+		return par;
+	}
+
+	public static String nombreCompleto(String nombre, String apellido) {
+		String nombreCompleto = nombre + apellido;
+		return nombreCompleto;
+	}
+
+	public static Float grados(Float grados, Float numero) {
+		Float fahrenheit = grados * numero;
+		return fahrenheit;
+	}
+
+	public static Integer numeroMayor(Integer n1, Integer n2, Integer n3) {
+		return null;
+	}
+
+	public static String generarContraseña(Integer tamaño) {
+		return null;
+	}
+
+	public static Boolean serPalindromo(String palabra) {
+		return null;
+	}
+
+	public static Double generarPotencia(Integer base, Integer exponente) {
+		return null;
+	}
+
+	public static Float generarMedia(Integer[] numeros) {
+		return null;
+	}
+
+	public static Integer numeroVocales(String palabra) {
+		return null;
+	}
+
+	public static void imprimirSaludo(String saludo) {
+	}
+
+	public static String ordenarCadenas(String[] cadenas) {
+		return null;
+	}
+
+	public static void menuOpciones() {
+	}
+
+}
