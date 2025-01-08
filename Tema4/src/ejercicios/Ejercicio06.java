ec7ae77b2f43f0fe402784a4d1cc7908e080f97c
LuisRamajoCeu <lramajo2587@ceuandalucia.es>
2025-01-08 20:43:30 +0100

    Ej 6

--- Tema4/src/ejercicios/Ejercicio06.java
+++ Tema4/src/ejercicios/Ejercicio06.java
@@ -1,9 +1,17 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio06 {
 
 	public static void main(String[] args) {
-	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un número: ");
+		Integer n1 = sc.nextInt();
+		System.out.println("Introduce otro número: ");
+		Integer n2 = sc.nextInt();
+		Integer resultado = Ejercicio05.sumar(n1, n2);
+		System.out.println("La suma es: " + resultado);
+		sc.close();
 	}
-
-}
+	}
